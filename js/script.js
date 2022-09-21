7f6b981d0c35b196b3ab45b1945e5c20e41acf85
rapha1010 <rapha.tavares1010@gmail.com>
2022-09-21 09:06:40 -0300

    last commit

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const nav = document.querySelector("header");
 let lastScrollY = window.scrollY;
 console.log(lastScrollY);
 
-window.addEventListener("scroll", ()=> {
+window.addEventListener("scroll", () => {
 
-    if(lastScrollY < window.scrollY) {
+    if (lastScrollY < window.scrollY) {
         nav.style["opacity"] = 1;
-        for (let scroll = 0; scroll < 100; scroll+=10) {
+        for (let scroll = 0; scroll < 100; scroll += 10) {
             if (window.scrollY > scroll) {
                 nav.style["opacity"] -= 0.1;
             }
@@ -18,3 +18,64 @@ window.addEventListener("scroll", ()=> {
     lastScrollY = window.scrollY;
 
 })
+
+
+let username = 'raphael';
+let password = '123456';
+
+var endPointUrlAddressLocal = "http://localhost:8081/api/itens";
+var endPointUrlAddress = "https://deliveryfoodapi.herokuapp.com/api/itens";
+
+var auth = "Basic " + btoa(username + ":" + password);
+
+var myInit = {
+    method: 'GET',
+    headers: {
+        'Authorization': auth
+    },
+    mode: 'cors',
+};
+
+
+var allItens = fetch(endPointUrlAddressLocal, myInit).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    printCard(data);
+}).catch(function (err) {
+    // There was an error
+    console.warn('Something went wrong.', err);
+});
+
+
+
+
+//Print Card
+function printCard(data) {
+
+    let content = document.getElementById("content");
+
+    data.forEach(element => {
+        content.innerHTML+=card(element);
+    });
+}
+
+function findOneIten(itenId) {
+    url = "/src/pages/product.html";
+    console.log(itenId);
+    // return itenId;
+}
+
+
+//Template Engine
+card = function ({id, description, price, imageUrl}) {
+
+    return `<div class="col-4 col-md-4">
+                <div class="card" style="width: 7rem;">
+                    <img src="${imageUrl}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${description} - R$ ${price}</h5>
+                <a onclick="findOneIten('${id}')" class="btn-comprar btn btn-warning">Comprar</a>
+                </div>
+                </div>
+            </div>`;
+}
---
+++ service-worker.js
@@ -0,0 +1,71 @@
+// 'use strict';
+
+// const CACHE_NAME = "BeerBeer_estatico";
+
+// const FILES_CACHE = [
+
+//     "../css/bootstrap.min.css",
+//     "../css/styles.css",
+//     "../img/bg.jpg",
+//     "../img/logo.png",
+//     "../js/script.js",
+//     "../js/bootstrap.bundle.min.js",
+//     "offline.html"
+
+// ];
+
+// //Instalar Service Worker
+// self.addEventListener("install", (evt) => {
+
+//     evt.waitUntil(
+
+//         caches.open(CACHE_NAME).then((cache) => {
+
+//             console.log("Service Worker gravando o cache estático.");
+//             return cache.addAll(FILES_CACHE);
+
+//         })
+
+//     );
+//     self.skipWaiting();
+
+// });
+
+// //Ativar Service Worker
+// self.addEventListener("activate", (evt) => {
+
+//     evt.waitUntil(
+
+//         caches.keys().then((keylist) => {
+
+//             return Promise.all(keylist.map((key) => {
+//                 if(key !== CACHE_NAME){
+//                     return caches.delete(key);
+//                 }
+//             }));
+
+//         })
+
+//     );
+//     self.clients.claim();
+// });
+
+// //Responder Off-line
+
+// self.addEventListener("fetch", (evt) => {
+
+//     if(evt.request.mode !== "navigate"){
+//         return;
+//     }
+
+//     evt.respondWith(
+//         fetch(evt.request).catch(()=>{
+//             return caches.open(CACHE_NAME).then((cache) =>{
+
+//                 return cache.match("offline.html");
+//             });
+
+//         })
+//     );
+
+// });
